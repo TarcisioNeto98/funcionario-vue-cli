493025dfa6cbfe186c76a55afa0360e749e1d5bc
TarcisioNeto98 <tarcisioneto1998@gmail.com>
2020-10-12 17:40:37 -0300

    Formulário de cadastro de recurso adicionado

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container" id="app">
+    <Formulario/>
+  </div>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Formulario
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,32 @@
+<template>
+    <form class="mt-3">
+        <h3>Formulário de Cadastro</h3>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="idNome">Nome</label><br>
+                <input id="idNome" class="form-control" type="text" placeholder="Digite seu nome">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="idCargo">Cargo</label><br>
+                <input id="idCargo" class="form-control" type="text" placeholder="Digite seu cargo">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="idNascimento">Data de Nascimento</label><br>
+                <input id="idNascimento" class="form-control" type="date" placeholder="Data de Nascimento">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="idEntrada">Nome</label><br>
+                <input id="idEntrada" class="form-control" type="date" placeholder="Data de Entrada">
+            </div>
+        </div>
+        <input type="button" class="btn btn-outline-success btn-lg" value="Post">
+    </form>
+</template>
+
+<script>
+export default {
+    name:  'Formulario'
+}
+</script>
